5d6f8a59a55f7793bd0d791246c51ad5ec674594
Jenish0517 <wolferjen@gmail.com>
2025-12-11 04:04:15 -0500

    add the app

---
+++ java app/src/test/java/apptest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
+
